58f269480efa605dd10f12c00dd434426cf4099c
Pavel Sanatov <psanatov@gmail.com>
2018-05-03 21:49:40 +0000

    Added client, that calls utils class, and synthesizes response as a output (text-to-speech)

---
+++ client.py
@@ -0,0 +1,29 @@
+# /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Simple implementation for outer neural network's usage.
+
+Sends request with desired text to host with neural network (in my case it was:
+http://eba.cloud:8080/talk endpoint).
+
+Retrieves data and then synthesizing it using my wrapper.
+
+"""
+import requests
+import json
+
+from speech import Speech # Our wrapper
+
+"""TODO: Wrap it up inside of a RESTful API
+so we can access synthesizer from outside of 
+our app (e.g. from mobile client).
+"""
+# Get input from user
+INPUT_DATA = raw_input("Type text to-be-processed by neural network: ")
+
+# Send POST request
+r = requests.post('http://eba.cloud:8080/talk', json = {'text':INPUT_DATA} )
+jr = json.loads(r.text)['answer'].encode('utf-8').strip()
+
+Speech.say(jr)
+
+print jr
