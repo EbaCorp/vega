2056045709cb33c8b075fcdc1cf1f2b16a76fff2
Pavel Sanatov <psanatov@gmail.com>
2018-05-03 20:03:59 +0000

    Renamed voice_test.py to speech.py. Wrapped into a class. Added static method annotation for external use.

---
+++ speech.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Speech Utilities.
+
+This module is responsible for any speech-to-text or text-to-speech
+interactions with outer APIs/services.
+"""
+
+
+from subprocess import call
+import time
+
+from espeak import espeak
+from espeak import core as espeak_core
+
+
+SPEECH_SPEED = 40 # words per minute
+
+espeak.set_parameter(espeak.Parameter.Rate, SPEECH_SPEED, False)
+espeak.set_voice("ru") # Russian language interface
+
+class Speech():
+    """Method for audio output.
+    
+    We are passing data to outer RESTful API, then getting back response.
+    Response is being synthesized. User hears output as an audio.
+    """
+    @staticmethod
+    def say(*args):
+        done_synth=[False]
+        def cb(event, pos, length):
+            if event == espeak_core.event_MSG_TERMINATED:
+                done_synth[0] = True
+        espeak.set_SynthCallback(cb)
+        r = espeak.synth(*args)
+
+        # TODO: Log everything into a separate file/directory
+        while r and not done_synth[0]:
+            time.sleep(0.05)
+        return r
